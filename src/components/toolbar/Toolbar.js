93f1115b71dc4d425ce11736170be06caded8823
mac <antonyblueeye@gmail.com>
2020-08-23 12:47:01 +0300

    Finish basic excel logic (additional changes)

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -6,7 +6,6 @@ export class Toolbar extends ExcelComponent {
     constructor($root, options) {
         super($root, {
             name: 'Toolbar',
-            listeners: ['click'],
             ...options
         })
     }
@@ -35,9 +34,5 @@ export class Toolbar extends ExcelComponent {
                 <div class="button">
                     <i class="material-icons">format_align_right</i>
                 </div>`
-    }
-
-    onClick(event) {
-        console.log(event.target)
     }
 }
